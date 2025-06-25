41f1429ec4a259625ff198ed14f0699a94588d61
Rikhu Surya <surya.rikhu@gmail.com>
2025-06-25 16:21:58 +0200

    Add Home page - list of blog posts

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export async function getStaticProps() {
+    const res = await fetch('http://localhost:3000/api/posts');
+    const posts = await res.json();
+    return { props: { posts } };
+}
+
+export default function Home({ posts }) {
+    return (
+        <div>
+            <h1>My Blog</h1>
+            {posts.map(post => (
+                <div key={post.id}>
+                    <Link href={`/posts/${post.id}`}>
+                        <h2>{post.title}</h2>
+                    </Link>
+                    <p>{post.description}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
